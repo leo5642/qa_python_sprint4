ca33b016b2a4af33d8b17d5d7a50a9c2fa4d6147
Lev Struin <lev_struin@mail.ru>
2025-06-09 15:07:31 +0100

    mk2

--- tests.py
+++ tests.py
@@ -1,14 +1,14 @@
 from main import BooksCollector
 
-# TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
+# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
-class TestBooksCollector:
+class TestBooksCollector1:
 
     # пример теста:
     # обязательно указывать префикс test_
     # дальше идет название метода, который тестируем add_new_book_
     # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
+    def test_add_new_book_add_two_books(self): #1
         # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
 
@@ -17,11 +17,12 @@ class TestBooksCollector:
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
         # проверяем, что добавилось именно две
-        # слooks_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
+        assert len(collector.get_books_genre()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
 
 class TestBooksCollector2:
     def test_set_book_genre_add_two_books_and_genre(self):#2
@@ -34,8 +35,8 @@ class TestBooksCollector2:
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         collector.set_book_genre('Что делать, если ваш кот хочет вас убить', 'Комедии')
 
-        assert collector.books_genre['Гордость и предубеждение и зомби'] ==  'Ужасы'
-        assert collector.books_genre['Что делать, если ваш кот хочет вас убить'] ==  'Комедии'
+        assert collector.get_books_genre()['Гордость и предубеждение и зомби'] ==  'Ужасы'
+        assert collector.get_books_genre()['Что делать, если ваш кот хочет вас убить'] ==  'Комедии'
 
 
 class TestBooksCollector3:
@@ -44,8 +45,9 @@ class TestBooksCollector3:
         collector = BooksCollector()
 
         collector.add_new_book('Гордость и предубеждение и зомби')
-    
-        assert collector.get_book_genre('Гордость и предубеждение и зомби') == 'Гордость и предубеждение и зомби'
+        collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
+
+        assert collector.get_books_genre()['Гордость и предубеждение и зомби'] == 'Ужасы'
 
 
 class TestBooksCollector4:
@@ -59,7 +61,7 @@ class TestBooksCollector4:
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         collector.set_book_genre('Что делать, если ваш кот хочет вас убить', 'Комедии')
 
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        collector.add_new_book('Горе от ума')
         collector.set_book_genre('Горе от ума', 'Комедии')
 
         assert len(collector.get_books_with_specific_genre('Ужасы')) == 1
@@ -72,9 +74,9 @@ class TestBooksCollector5:
         
         collector = BooksCollector()
 
-        collector.get_books_genre
+        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
-        assert books_genre != ''
+        assert len(collector.get_books_genre()) == 1
 
 
 class TestBooksCollector6:
@@ -88,8 +90,8 @@ class TestBooksCollector6:
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         collector.set_book_genre('Что делать, если ваш кот хочет вас убить', 'Комедии')
 
-        assert collector.get_books_for_children in 'Что делать, если ваш кот хочет вас убить'
-        assert not collector.get_books_for_children in 'Гордость и предубеждение и зомби'
+        assert  'Что делать, если ваш кот хочет вас убить' in collector.get_books_for_children()
+        assert 'Гордость и предубеждение и зомби' not in collector.get_books_for_children()
 
 
 class TestBooksCollector7:
@@ -100,7 +102,7 @@ class TestBooksCollector7:
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_book_in_favorites('Гордость и предубеждение и зомби')
 
-        assert collector.favorites == 'Гордость и предубеждение и зомби'
+        assert 'Гордость и предубеждение и зомби' in collector.favorites
 
 
 class TestBooksCollector8:
@@ -112,14 +114,13 @@ class TestBooksCollector8:
         collector.add_book_in_favorites('Гордость и предубеждение и зомби')
         collector.delete_book_from_favorites('Гордость и предубеждение и зомби')
 
-        assert collector.favorites == ''
+        assert collector.favorites == []
 
 
 class TestBooksCollector9:
     def test_get_list_of_favorites_books(self):#9
 
         collector = BooksCollector()
-        collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_book_in_favorites('Гордость и предубеждение и зомби')
 
-        assert collector.favorites == 'Гордость и предубеждение и зомби'
+        assert collector.favorites == []
